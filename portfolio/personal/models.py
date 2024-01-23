52f400063b5e49d3dd8c9d7ea31974d2cff120b0
Midhun Balachandran <midhunbalachandran07@gmail.com>
2024-01-23 17:54:55 +0530

    feat: Add view function for message handling and database storage

--- portfolio/personal/models.py
+++ portfolio/personal/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactForm(models.Model):
+  name = models.CharField(max_length=100)
+  email = models.EmailField()
+  subject = models.CharField(max_length=100)
+  message = models.TextField()
--- portfolio/personal/views.py
+++ portfolio/personal/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ContactForm
+from django.views.decorators.csrf import csrf_exempt
 
 
 def home(request):
   return render(request , 'personal.html')
+
+@csrf_exempt
+def contact(request):
+  if request.method == 'POST':
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    subject = request.POST.get('subject')
+    message = request.POST.get('message')
+    # Save form data to database
+    form = ContactForm(name=name, email=email, subject=subject, message=message)
+    form.save()
+    return HttpResponse('Your message has been received. Thank you!')
+  else:
+    return HttpResponse('Invalid request.')
